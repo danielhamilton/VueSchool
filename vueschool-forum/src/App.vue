d9a35d1a9268ebd10db36cdb3f00a5ab696ccc7f
danielhamilton <307828+danielhamilton@users.noreply.github.com>
2022-11-03 02:33:50 -0400

    initial commit. PageHome setup and dummy data

---
+++ vueschool-forum/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <page-home />
+</template>
+
+<script>
+import PageHome from "@/components/PageHome.vue";
+
+export default {
+  name: "App",
+  components: {
+    PageHome,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Inter, Helvetica-Neue, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  width: 50%;
+  margin: 0 auto;
+}
+</style>
---
+++ vueschool-forum/src/components/PageHome.vue
@@ -0,0 +1,45 @@
+<template>
+  <div><h1>Hello from PageHome</h1></div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>
+        {{
+          users.find((u) => u.id === posts.find((p) => p.id === postId).userId)
+            .name
+        }}
+      </p>
+      <p>{{ posts.find((p) => p.id === postId).text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from "@/data.json";
+export default {
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users,
+    };
+  },
+};
+</script>
+
+<style lang="css" scoped>
+h1 {
+  font-size: 2rem;
+  font-weight: 900;
+}
+h2 {
+  font-size: 1.25rem;
+  font-weight: 900;
+  color: #8d1bd3;
+}
+p {
+  font-size: 1rem;
+  line-height: 1.5rem;
+  text-align: left;
+}
+</style>

708e65850d96d256a6bdae60bc53472d8360cd00
danielhamilton <307828+danielhamilton@users.noreply.github.com>
2022-11-03 22:36:30 -0400

    added methods, clean PageHome

--- vueschool-forum/src/components/HelloWorld.vue
+++ vueschool-forum/src/components/HelloWorld.vue
@@ -88,35 +88,20 @@
 </template>
 
 <script>
-  import sourceData from "@/data.json";
-  console.log(sourceData);
-  export default {
-    name: "HelloWorld",
-    props: {
-      msg: String,
-    },
-    data() {
-      return {
-        threads: sourceData.threads,
-      };
-    },
-  };
+import sourceData from "@/data.json";
+console.log(sourceData);
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      threads: sourceData.threads,
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
-  }
-</style>
+<style scoped></style>

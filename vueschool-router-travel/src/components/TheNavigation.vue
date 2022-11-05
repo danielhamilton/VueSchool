cef313fcb967583b5d72cf7fc2dab5b1b9a8e2e0
danielhamilton <307828+danielhamilton@users.noreply.github.com>
2022-11-05 00:51:19 -0400

    removed hard coded destinations, updated nav route

--- vueschool-router-travel/src/components/TheNavigation.vue
+++ vueschool-router-travel/src/components/TheNavigation.vue
@@ -1,27 +1,66 @@
 <template>
   <nav id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
-    <router-link to="/panama">Panama</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
+    <p class="logo">Getaways</p>
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to="/"> Home </router-link>
+      </li>
+      <li
+        v-for="destination in destinations"
+        :key="destination.name"
+        class="links"
+      >
+        <router-link
+          :to="{
+            name: 'DestinationDetails',
+            params: { id: destination.id },
+          }"
+        >
+          {{ destination.name }}
+        </router-link>
+      </li>
+    </ul>
   </nav>
 </template>
+
+<script>
+import store from "@/store";
+export default {
+  data() {
+    return {
+      destinationId: this.$route.params.id,
+      destinations: store.destinations,
+    };
+  },
+};
+</script>
 
 <style scoped>
 #nav {
   display: flex;
-  justify-content: center;
-  padding: 30px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
-  padding: 0 10px;
+  text-decoration: none;
 }
-
 #nav a.router-link-exact-active {
   color: #8721d5;
+}
+.nav-links {
+  display: flex;
+}
+.links {
+  padding-right: 20px;
+  list-style: none;
+}
+.links:hover {
+  text-decoration: underline;
+}
+
+.logo {
+  font-size: 20px;
+  color: #8721d5;
+  font-weight: 900;
 }
 </style>
--- vueschool-router-travel/src/router/index.js
+++ vueschool-router-travel/src/router/index.js
@@ -7,36 +7,7 @@ const routes = [
     name: "home",
     component: HomeView,
   },
-  {
-    path: "/about",
-    name: "about",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
-    path: "/brazil",
-    name: "brazil",
-    component: () =>
-      import(/* webpackChunkName: "brazil" */ "../views/BrazilView.vue"),
-  },
-  {
-    path: "/hawaii",
-    name: "hawaii",
-    component: () =>
-      import(/* webpackChunkName: "hawaii" */ "../views/HawaiiView.vue"),
-  },
-  {
-    path: "/jamaica",
-    name: "jamaica",
-    component: () =>
-      import(/* webpackChunkName: "jamaica" */ "../views/JamaicaView.vue"),
-  },
-  {
-    path: "/panama",
-    name: "panama",
-    component: () =>
-      import(/* webpackChunkName: "brazil" */ "../views/PanamaView.vue"),
-  },
+
   {
     path: "/details/:id",
     name: "DestinationDetails",
